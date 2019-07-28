3a81eaade4ed09ec0cfd458dcd3b34c672677ae6
Agridho Ilhamantama <agridhoi@gmail.com>
2019-07-28 17:20:05 +0200

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-triangular-40957',
-    user : 'postgres',
-    password : 'fizz2211',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
